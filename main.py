b8665c70d339221831e94ffc1bbda0b4ef6df361
unknown <sullivant1111@gmail.com>
2019-05-28 15:08:16 -0400

    Done again

--- main.py
+++ main.py
@@ -13,24 +13,39 @@ counter = 1
 notebook = []
 now = date.today()
 
-while True:
+def menu():
     user_response = input ("What would you like to do? \n"
                             "1. Add a note\n"
                             "2. Print a note\n"
                             "3. Exit\n>")
-    if user_response == "1":
-        content = input("What is the note \n> ")
-        note_id = counter
-        note = (note_id, str(now), content)
-        notebook.append(note)
-        counter += 1
-    elif user_response == "2":
-        for note in notebook:
-            print(f"Id: {note[0]}| {note[2]}")
-    elif user_response == "3":
-        exit()  
-    else:
-        print("Invalid input")    
+    return user_response
+
+def note_create():
+    content = input("What is the note \n> ")
+    global counter
+    note_id = counter
+    note = (note_id, str(now), content)
+    notebook.append(note)
+    counter += 1
+
+def print_notes():
+    for note in notebook:
+         print(f"Id: {note[0]}| {note[2]}")
+
+def run():
+    while True:
+        choice = menu ()
+        if choice == "1":
+            note_create()
+        elif choice == "2":
+            print_notes()
+        elif choice == "3":
+            exit()  
+        else:
+            print("Invalid input")    
+
+if __name__=="__main__":
+    run()
 
 # print(notebook)
 # user_response = input ("Would you like to make a note? \n Y, N >").lower()
